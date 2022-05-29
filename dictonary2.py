aaca0064a832301d55a3e27d40f386cb5ae88801
Rahul Goyal <103593172+Rahul2115000799@users.noreply.github.com>
2022-05-29 07:23:13 -0700

    Create dictonary2.py

---
+++ dictonary2.py
@@ -0,0 +1,10 @@
+# dictonary 2nd question
+# dictonary in python
+# keys and values
+
+dt = {'a':'juice','b':'mango','c':'banana'}
+for i in dt:
+    print(i,dt[i])
+
+ #for i,value in dt.items():
+ #   print(i,value)
